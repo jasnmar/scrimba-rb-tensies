a09c99cbedb8b9ed8da6e00cfab8957ac511dde4
jasnmar <jasnmar@gmail.com>
2024-04-22 10:12:59 -0400

    Getting Started

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import './App.css'
-
+/**
+ * Challenge: Start a brand new React app!
+ * - Create a separate App component
+ * - Import and render the App component here
+ * - In the App component, render a <main> element
+ * - Style everything to look like the slide
+ */
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>App running</h1>
+      <h1>App Running</h1>
     </>
   )
 }
