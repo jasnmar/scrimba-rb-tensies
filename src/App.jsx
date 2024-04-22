bb28f334673b4a5d3e31623ee330487258ef71b4
jasnmar <jasnmar@gmail.com>
2024-04-22 12:18:46 -0400

    Update isHeld values

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,15 @@ import { nanoid } from "nanoid"
 
 
 /**
- * Challenge: Create a function `holdDice` that takes
- * `id` as a parameter. For now, just have the function
- * console.log(id).
+ * Challenge: Update the `holdDice` function to flip
+ * the `isHeld` property on the object in the array
+ * that was clicked, based on the `id` prop passed
+ * into the function.
  * 
- * Then, figure out how to pass that function down to each
- * instance of the Die component so when each one is clicked,
- * it logs its own unique ID property. (Hint: there's more
- * than one way to make that work, so just choose whichever
- * you want)
- * 
+ * Hint: as usual, there's > 1 way to accomplish this.
+ * I'll be using `dice.map()` and checking for the `id`
+ * of the die to determine which one to flip `isHeld` on,
+ * but you can do whichever way makes the most sense to you.
  */
 
 function App() {
@@ -54,6 +53,15 @@ function App() {
 
   function holdDice(diceId) {
     console.log('diceId: ', diceId)
+    const newDiceObjects = diceObjects.map(diceObject => {
+      if(diceObject.id === diceId) {
+        diceObject.isHeld = !diceObject.isHeld  
+      }
+      return diceObject
+      
+    })
+    console.log('newDiceObjects: ', newDiceObjects)
+    setDiceObjects(newDiceObjects) 
   }
   return (
     <main>
