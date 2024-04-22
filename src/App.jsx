1667d8630e5b488fcb72ba4d68897ef20ff74475
jasnmar <jasnmar@gmail.com>
2024-04-22 10:51:57 -0400

    Rolling Dice

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,25 @@ import Die from './components/Die/Die'
 /**
  * Challenge:
  * 
- * - Create a Die component that takes a `value` prop
- * - Render 10 instances of the Die component (manually)
- *      - Provide a number between 1-6 for the value on each
- *        for now
- * - Style the <main> and <Die> components 
- *   to look like they do in the slide
- *      - Hints: Create a container to hold the 10 instances
- *        of the Die component, and use CSS Grid to lay them
- *        out evenly in 2 rows of 5 columns
- *      - Use flexbox on main to center the dice container
- *        in the center of the page
+ * Write a function (allNewDice) that returns an array 
+ * of 10 random numbers between 1-6 inclusive.
+ * 
+ * Log the array of numbers to the console for now
  */
 function App() {
   const [count, setCount] = useState(0)
+
+  console.log('allNewDice: ', allNewDice(10))
+  function allNewDice(numberToGenerate) {
+    const dice = []
+    for(let i=0; i < numberToGenerate; i++) {
+      dice.push(getRandomInt(6))
+    }
+    return dice
+  }
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max)+1;
+  }
 
   return (
     <main>
