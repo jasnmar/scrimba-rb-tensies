9b03916f3e7100d6418690a6916770672487d799
jasnmar <jasnmar@gmail.com>
2024-04-22 10:41:39 -0400

    Dice and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,40 @@
 import { useState } from 'react'
 import './App.css'
+import Die from './components/Die/Die'
+
+
 /**
- * Challenge: Start a brand new React app!
- * - Create a separate App component
- * - Import and render the App component here
- * - In the App component, render a <main> element
- * - Style everything to look like the slide
+ * Challenge:
+ * 
+ * - Create a Die component that takes a `value` prop
+ * - Render 10 instances of the Die component (manually)
+ *      - Provide a number between 1-6 for the value on each
+ *        for now
+ * - Style the <main> and <Die> components 
+ *   to look like they do in the slide
+ *      - Hints: Create a container to hold the 10 instances
+ *        of the Die component, and use CSS Grid to lay them
+ *        out evenly in 2 rows of 5 columns
+ *      - Use flexbox on main to center the dice container
+ *        in the center of the page
  */
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <main>
-      <h1>App Running</h1>
+      <div className='app--die-container'>
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+      </div>
     </main>
   )
 }
---
+++ src/components/Die/Die.jsx
@@ -0,0 +1,10 @@
+import "./Die.css"
+
+function Die(props) {
+    return (
+        <div className="die--die-body">{props.value}</div>
+    )
+
+}
+
+export default Die
